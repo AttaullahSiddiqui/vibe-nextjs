c9d9741355bef7d823148ebc02944100a606e4b0
Attaullah Siddiqui <devmeanstack93@gmail.com>
2025-06-29 00:34:59 +0500

    03-trpc-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
+import { trpc, getQueryClient } from "@/trpc/server";
+
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+
+import { Suspense } from "react";
+
+import { Client } from "./client";
+
 export default async function Page() {
-  return <div>Hello world!</div>;
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(
+    trpc.createAI.queryOptions({ text: "Atta prefetch" })
+  );
+  // const data = await caller.createAI({ text: "Atta server" });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Client />
+      </Suspense>
+    </HydrationBoundary>
+  );
+  // return <div>{JSON.stringify(data)}</div>;
 }
